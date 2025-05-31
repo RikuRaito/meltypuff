7488630aca8775de7898ef55b0f8567ea1f43058
斉藤吏功 <riku@MacBook-Air.local>
2025-05-31 11:10:54 +0900

    building  infrastructure all done

---
+++ frontend/src/Top.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './Top.css'
+
+function Top() {
+
+  return (
+    <div top-container>
+      <p>トップページ</p>
+    </div>
+  )
+}
+
+export default Top
